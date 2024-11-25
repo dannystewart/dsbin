1f33df1484e97f2b6844d8634e98a2ccd0ca889e
Danny Stewart <danny@stewart.cc>
2024-11-25 08:27:06 -0500

    pybounce (enhancement): Add async keyboard interrupt handling and update script invocation path

--- src/dsbin/music/pybounce.py
+++ src/dsbin/music/pybounce.py
@@ -4,7 +4,7 @@
 Uploads audio files to a Telegram channel.
 
 To have this run automatically via Hazel, call it as an embedded script like this:
-    ❯ source ~/.zshrc && $(pyenv which python) -m dsbin.pybounce "$1"
+    ❯ source ~/.zshrc && $(pyenv which python) -m dsbin.music.pybounce "$1"
 """
 
 from __future__ import annotations
@@ -32,6 +32,7 @@ from dsutil.env import DSEnv
 from dsutil.log import LocalLogger
 from dsutil.macos import get_timestamps
 from dsutil.paths import DSPaths
+from dsutil.shell import handle_async_keyboard_interrupt
 from dsutil.tools import async_retry_on_exception
 
 env = DSEnv("pybounce")
@@ -262,6 +263,7 @@ class TelegramUploader:
             logger.warning("Skipping '%s'.", file)
 
 
+@handle_async_keyboard_interrupt()
 async def run() -> None:
     """Upload files to a Telegram channel."""
     # Parse command-line arguments

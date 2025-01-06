eb030742349f32e0094eaa698cf87b2ea41d4f36
Danny Stewart <danny@stewart.cc>
2025-01-06 16:43:51 -0500

    dupefinder (fix): Use pathlib for file listing and extract main function

--- src/dsbin/files/dupefinder.py
+++ src/dsbin/files/dupefinder.py
@@ -7,17 +7,23 @@ This script will find duplicate files in a directory and print them to the conso
 
 from __future__ import annotations
 
-import os
 import sys
+from pathlib import Path
 
 from dsutil import configure_traceback
 from dsutil.files import find_duplicate_files_by_hash
 
 configure_traceback()
 
-if __name__ == "__main__":
+
+def main() -> None:
+    """Find duplicate files in a directory."""
     input_files = sys.argv[1:]
     if len(input_files) == 0:
-        input_files = [f for f in os.listdir(".") if os.path.isfile(f)]
+        input_files = [f for f in Path().iterdir() if f.is_file()]
 
     find_duplicate_files_by_hash(input_files)
+
+
+if __name__ == "__main__":
+    main()

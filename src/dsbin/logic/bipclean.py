578fbdc998bf58cd3676f2a4abe62d7eb9c2e738
Danny Stewart <danny@stewart.cc>
2025-01-20 11:20:06 -0500

    bipclean (tweak): Fix pluralization in output; switch to pathlib; update default hours to 2

--- src/dsbin/logic/bipclean.py
+++ src/dsbin/logic/bipclean.py
@@ -1,4 +1,4 @@
-"""Identifies and deletes AIFF files created within a specified time period (default 24 hours).
+"""Identifies and deletes AIFF files created within a specified time period (default 2 hours).
 
 This script is for audio files bounced in place within a Logic project. These files end up in the
 Audio Files folder, but if you decide to revert or save the project without keeping it, they're not
@@ -11,8 +11,8 @@ value by using the `--hours` argument. Files are listed and confirmed before del
 from __future__ import annotations
 
 import argparse
-import os
 from datetime import datetime, timedelta
+from pathlib import Path
 from zoneinfo import ZoneInfo
 
 from dsutil import configure_traceback
@@ -22,14 +22,17 @@ from dsutil.text import print_colored
 
 configure_traceback()
 
-DEFAULT_HOURS = 4
+DEFAULT_HOURS = 2
 
 
 def parse_args() -> argparse.Namespace:
     """Parse arguments passed in from the command line."""
     parser = argparse.ArgumentParser(description="find and delete recent AIFF files")
     parser.add_argument(
-        "--hours", type=int, default=DEFAULT_HOURS, help="hours to look back (default: 4)"
+        "--hours",
+        type=int,
+        default=DEFAULT_HOURS,
+        help=f"hours to look back (default: {DEFAULT_HOURS})",
     )
     return parser.parse_args()
 
@@ -37,24 +40,25 @@ def parse_args() -> argparse.Namespace:
 def main() -> None:
     """Find and delete AIFF files created within the specified time period."""
     args = parse_args()
-    current_dir = os.getcwd()
-    time_duration = datetime.now(tz=ZoneInfo("America/New_York")) - timedelta(hours=args.hours)
+    hours = args.hours
+    current_dir = str(Path.cwd())
+    duration = datetime.now(tz=ZoneInfo("America/New_York")) - timedelta(hours=hours)
 
-    # List AIFF files created within the specified time period
-    aif_files = list_files(directory=current_dir, extensions=["aif"], modified_after=time_duration)
+    aiff_files = list_files(directory=current_dir, extensions=["aif"], modified_after=duration)
 
-    if not aif_files:  # If no files are found, exit
-        print_colored(f"No AIFF files from within the last {args.hours} hours.", "green")
-        if args.hours == DEFAULT_HOURS:
-            print_colored("You can use --hours to specify a different time period.", "cyan")
+    s = "s" if hours != 1 else ""
+    if not aiff_files:
+        print_colored(f"No AIFF files from within the last {hours} hour{s}.", "green")
+        if hours == DEFAULT_HOURS:
+            print_colored("Use --hours to specify a different time period.", "cyan")
         return
 
-    print_colored(f"The following AIFF files were created in the last {args.hours} hours:", "green")
-    for file in aif_files:
+    print_colored(f"The following AIFF files were created in the last {hours} hour{s}:", "green")
+    for file in aiff_files:
         print(f"- {file}")
 
     if confirm_action("Delete the files?", default_to_yes=True):
-        delete_files(aif_files)
+        delete_files(aiff_files)
     else:
         print("No files were deleted.")
 

7660c0b01231d262a39045665673609612d1d53e
Danny Stewart <danny@stewart.cc>
2025-03-18 23:55:33 -0400

    setmag (refactor): Use LocalLogger and add platform check

--- src/dsbin/mac/setmag.py
+++ src/dsbin/mac/setmag.py
@@ -4,44 +4,67 @@
 
 from __future__ import annotations
 
-import os
 import subprocess
-import sys
 
-from dsbase.util import dsbase_setup
+from dsbase.log import LocalLogger
+from dsbase.util import dsbase_setup, platform_check
 
 dsbase_setup()
+platform_check("Darwin")
+
+logger = LocalLogger().get_logger("setmag")
 
 
-def log_message(message: str) -> None:
-    """Log a message to stdout or syslog."""
-    if sys.stdout.isatty():
-        print(message)
+def main() -> None:
+    """Run the main script functionality."""
+    output = subprocess.getoutput("pmset -g batt")
+
+    if "Now drawing from 'AC Power'" in output and "AC attached; not charging" in output:
+        logger.info("Connected to power but not charging, so setting MagSafe to green")
+        subprocess.run(
+            [
+                "/usr/local/bin/gtimeout",
+                "3s",
+                "sudo",
+                "/usr/local/bin/smc",
+                "-k",
+                "ACLC",
+                "-w",
+                "03",
+            ],
+            check=False,
+        )
+    elif "Now drawing from 'AC Power'" in output:
+        logger.info("Connected to power and charging, resetting MagSafe to default behavior")
+        subprocess.run(
+            [
+                "/usr/local/bin/gtimeout",
+                "3s",
+                "sudo",
+                "/usr/local/bin/smc",
+                "-k",
+                "ACLC",
+                "-w",
+                "00",
+            ],
+            check=False,
+        )
     else:
-        subprocess.run(["logger", "-p", "user.info", message], check=False)
+        logger.info("Unable to determine status, resetting MagSafe to default behavior")
+        subprocess.run(
+            [
+                "/usr/local/bin/gtimeout",
+                "3s",
+                "sudo",
+                "/usr/local/bin/smc",
+                "-k",
+                "ACLC",
+                "-w",
+                "00",
+            ],
+            check=False,
+        )
 
 
-if os.uname().sysname != "Darwin":  # type: ignore
-    log_message("This can only be run on macOS. Aborting.")
-    sys.exit(1)
-
-output = subprocess.getoutput("pmset -g batt")
-
-if "Now drawing from 'AC Power'" in output and "AC attached; not charging" in output:
-    log_message("Connected to power but not charging, so setting MagSafe to green")
-    subprocess.run(
-        ["/usr/local/bin/gtimeout", "3s", "sudo", "/usr/local/bin/smc", "-k", "ACLC", "-w", "03"],
-        check=False,
-    )
-elif "Now drawing from 'AC Power'" in output:
-    log_message("Connected to power and charging, resetting MagSafe to default behavior")
-    subprocess.run(
-        ["/usr/local/bin/gtimeout", "3s", "sudo", "/usr/local/bin/smc", "-k", "ACLC", "-w", "00"],
-        check=False,
-    )
-else:
-    log_message("Unable to determine status, resetting MagSafe to default behavior")
-    subprocess.run(
-        ["/usr/local/bin/gtimeout", "3s", "sudo", "/usr/local/bin/smc", "-k", "ACLC", "-w", "00"],
-        check=False,
-    )
+if __name__ == "__main__":
+    main()

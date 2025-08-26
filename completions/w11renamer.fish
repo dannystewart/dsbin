65129542f5a2b4eded21530bd5ca21aafe555abb
Danny Stewart <danny@stewart.cc>
2025-08-26 16:55:36 -0400

    fix(fish): let w11renamer get filenames
    Removes the -f flag from the completion definition.

--- completions/w11renamer.fish
+++ completions/w11renamer.fish
@@ -1,2 +1,1 @@
-# Auto-generated completions for w11renamer
-complete -c w11renamer -d "windows 11 ISO filename or string to process" -f
+complete -c w11renamer -d "Windows 11 ISO filename or string to process"
